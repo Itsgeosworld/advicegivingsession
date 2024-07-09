a26f67e4bce10f544a372654650aeed2e01f4c8d
Itsgeosworld <gcrudup@my.waketech.edu>
2024-07-08 20:43:24 -0400

    Main program
    Still in a working progress. Firstly, the program asks whether the user are aware of well known fears that can be detrimental to our well-being.

---
+++ main.py
@@ -0,0 +1,85 @@
+print('Welcome to the Inner Child Session!')
+first_name = input('What is your first name? ')
+last_name_initial = input('What is the initial for your last name? ')
+age = int(input('What is your age? '))
+points = 0
+
+print(f'Hello {first_name} {last_name_initial} you are {age} years old.')
+
+if age >= 18:
+    print(f'{first_name}, you are old enough to start this session!')
+
+    response_to_join_session = input('Are you ready to begin this session? ').lower()
+    if response_to_join_session == 'y':
+        print(f'Lets begin this session! You currently have {points} points.')
+        print('Firstly, you will be asked questions on types of fears you are familiar with or experienced.')
+        print('Each fear you are familiar with you will earn 5 points. If not, you will lose 5 points!')
+
+
+    first_question = input('Are you aware of what fear of rejection is? (y/n) ')
+    if first_question == 'n':
+        print('Fear of rejection can stem from not being accepted. Getting turned down for a romantic date is an example.')
+        points -= 5
+        print(f'You now have {points} points...')
+    elif first_question == 'y':
+        print('Great! You are aware of what fear of rejection is.')
+        points += 5
+        print(f'You now have earned {points} points!')
+
+
+    second_question = input('Are you aware of what fear of failure is? (y/n) ')
+    if second_question == 'n':
+        print('Fear of failure stems from not wanting to do poorly in aspects of your life.')
+        points -= 5
+        print(f'You now have {points} points...')
+    elif second_question == 'y':
+        print('Great! You are aware of what fear of failure is.')
+        points += 5
+        print(f'You now have earned {points} points!')
+
+
+    third_question = input('Are you aware of what fear of judgement is? (y/n) ')
+    if third_question == 'n':
+        print('Fear of judgement stems from how others or yourself view you.')
+        points -= 5
+        print(f'You now have {points} points...')
+    elif third_question == 'y':
+        print('Great! You are aware of what fear of judgement is.')
+        points += 5
+        print(f'You have earned {points} points!')
+
+
+    fourth_question = input('Are you aware of what fear of success is? (y/n) ')
+    if fourth_question == 'n':
+        print('Fear of success can stem from obstacles you might have to endure to receive success.')
+        points -= 5
+        print(f'You now have {points} points...')
+    elif fourth_question == 'y':
+        print('Great! You are aware of what fear of success is.')
+        points += 5
+        print(f'You now have earned {points} points!')
+
+
+    fifth_question = input('Are you aware of what fear of change is? (y/n) ')
+    if fifth_question == 'n':
+        print('Fear of change can stem from not wanting to adapt to new things.')
+        points -= 5
+        print(f'You now have {points} points...')
+    elif fifth_question == 'y':
+        print('Great! You are aware of what fear of change is.')
+        points += 5
+        print(f'You now have earned {points} points!')
+
+
+    sixth_question = input('Are you aware of what fear of loss is? (y/n) ')
+    if sixth_question == 'n':
+        print('Fear of loss stems from not wanting to lose the people who are close to you.')
+        points -= 5
+        print(f'You now have {points} points...')
+    elif sixth_question == 'y':
+        print('Great! You are aware of what fear of loss is.')
+        points += 5
+        print(f'You have earned {points} points!')
+
+    print(f'You have answered the questions related to fears! You now have {points} points in total.')
+
